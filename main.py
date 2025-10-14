af40bdb4f65d06ae82b2f572be027fd459a9df9f
Hassan <itsmywork1019@gmail.com>
2025-10-14 19:49:34 +0500

    add vercel material

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from typing import Annotated, List, Optional
 from datetime import timedelta
 from fastapi.middleware.cors import CORSMiddleware
 import models, schemas, crud, auth, database
+from mangum import Mangum
 
 models.Base.metadata.create_all(bind=database.engine)
 
@@ -22,6 +23,7 @@ origins = [
 
 
 app = FastAPI(title='User Management API')
+handler = Mangum(app)
 
 app.add_middleware(
     CORSMiddleware,
